99a4795ec9b073d02e3624772f7a590a1f7b3ac9
Roman <roman.anokhin.work@gmail.com>
2025-08-14 15:20:28 +0300

    Update task 1-3

--- js/task-3.js
+++ js/task-3.js
@@ -19,9 +19,12 @@ class StringBuilder {
 
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
+
 builder.padStart("^");
 console.log(builder.getValue()); // "^."
+
 builder.padEnd("^");
 console.log(builder.getValue()); // "^.^"
+
 builder.padBoth("=");
 console.log(builder.getValue()); // "=^.^="
